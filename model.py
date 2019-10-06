93d0be60a0afd3617fec540f60439fa739e6a697
Nastaran <nsaffar@gmail.com>
2019-10-07 09:54:13 +1300

    Removing vgg_face_weights.h5 and changing model

--- model.py
+++ model.py
@@ -1,15 +1,77 @@
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D, Input, BatchNormalization
-from keras.models import  Model
+#from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, BatchNormalization
+#from keras.models import  Model
+from keras.models import Model, Sequential
+from keras.layers import Input, Convolution2D, ZeroPadding2D, MaxPooling2D, \
+     Flatten, Dense, Dropout, Activation, BatchNormalization
+from keras import optimizers
+
+def model_2():
+    model = Sequential()
+    model.add(ZeroPadding2D((1,1),input_shape=(224,224, 3)))
+    model.add(Convolution2D(64, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(Convolution2D(4096, (7, 7), activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Convolution2D(4096, (1, 1), activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Convolution2D(2622, (1, 1)))
+    model.add(Flatten())
+    model.add(Activation('softmax'))
+    model.load_weights('utils/vgg_face_weights.h5')
+    num_classes = 7
+    #Flatten and output
+    model.add(Flatten)
+    model.add(Activation('softmax'))
+    model.add(Dense(num_classes))
+    model.add(Activation('sigmoid'))
+
+    # compile the model
+    model.compile(loss = "categorical_crossentropy",
+                  optimizer = optimizers.SGD(lr=0.0001, momentum=0.9),
+                  metrics=["accuracy"])
+
+    return model
 
 def conv_model(input_shape=(48,48, 1)):
     '''
     Base model was in the Kaggle
     '''
     # first input model
-
     visible = Input(shape=input_shape, name='input')
-    print(visible)
     num_classes = 7
     #the 1-st block
     conv1_1 = Conv2D(64, kernel_size=3, activation='relu', padding='same', name = 'conv1_1')(visible)
@@ -71,6 +133,12 @@ def conv_model(input_shape=(48,48, 1)):
 
     # create model
     model = Model(inputs =visible, outputs = ouput)
+
+
+    # compile the model
+    model.compile(loss = "categorical_crossentropy",
+                  optimizer = optimizers.Adam(learning_rate=0.001),
+                  metrics=["accuracy"])
     # summary layers
     print(model.summary())
 
